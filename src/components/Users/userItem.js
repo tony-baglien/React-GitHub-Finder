151e1f621684c5684fe93f0423ec3b1471adc7f4
TOny Baglien <tony.b.composition@gmail.com>
2021-05-24 16:52:11 -0500

    finished lesson 1, worked with async functions

---
+++ src/components/Users/userItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const userItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login} </h3>
+
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm'>
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+userItem.propTypes = {
+  users: PropTypes.object.isRequired,
+};
+
+export default userItem;
